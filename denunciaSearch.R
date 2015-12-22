847be17f11f86b49adb61cb70340a3a5c62e214b
Patrick Emedom-Nnamdi <patricknnamdi2@gmail.com>
2015-12-22 13:51:12 -0500

    Denuncia Search for P1 and P2

--- denunciaSearch.R
+++ denunciaSearch.R
@@ -6,12 +6,11 @@ inspecciones <- inspecciones[, c("UNICODE.", "SITUACION","NRO_DENUNCIA","DIA", "
 rociado <- data.frame(read.csv("/Users/patrickemedom/Desktop/denuncia/rociado - ROCIADO.csv"))
 rociado <- rociado[, c("UNICODE", "SITUACION", "DIA", "MES", "ANIO", "IN_TCAP_TOT", "PD_TCAP_TOT")]
 
+
 ## merge datasets 
 colnames(inspecciones)[1] <- "UNICODE"
 dataset <- merge(inspecciones, rociado, by = "UNICODE", all = TRUE)
 
-## TODO write in to function of no. 
-denunVal <- inspecciones$SITUACION[inspecciones$NRO_DENUNCIA == i]
 
 ## Sum contents of IN_TCAP_TOT and PD_TCAP_TOT and replace NA with zeros 
 inspecciones$IN_TCAP_TOT <- ifelse(is.na(inspecciones$IN_TCAP_TOT), 0, inspecciones$IN_TCAP_TOT)
@@ -22,6 +21,7 @@ inspecciones$sumTotal <- ifelse(is.na(inspecciones$sumTotal), 0, inspecciones$su
 
 ## replace $Situacion with only positive contents (sumtotal > 0)
 indPos <- inspecciones[which(inspecciones$sumTotal != 0), ]
+
 indPos <- indPos[which(indPos$ANIO >= 2011),]
 
 ## function for total number of P1 or P2 for given denuncia no 
